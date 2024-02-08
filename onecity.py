9ac97d06c3488444f3e5cd7b5832e464a34f0256
kravchenski <max5045955@gmail.com>
2024-02-08 17:03:52 +0300

    feat: add a function verification_by_phone_erip

--- onecity.py
+++ onecity.py
@@ -8,6 +8,7 @@ from audioplayer import AudioPlayer
 from config import secret_key_gmail, city
 from form import fill_form, auth, check_dates
 from send_mail import send_mail
+from verification import verification_by_phone_erip
 
 while True:
     page = ChromiumPage()
@@ -19,7 +20,7 @@ while True:
         tag_info = page.ele('xpath://div[contains(@class,"border-info mb-0 ng-star-inserted")]').text
         if tag_info != 'Приносим извинения, в настоящий момент нет доступных слотов для записи. Пожалуйста, попробуйте позже':
             print('Send mail....')
-            send_mail(secret_key_gmail, city)
+            # send_mail(secret_key_gmail, city)
             print('Done')
             time.sleep(2)
             print('Play Audio...')
@@ -28,6 +29,7 @@ while True:
             page.ele('xpath:/html/body/app-root/div/div/app-eligibility-criteria/section/form/mat-card[2]/button').click()
             time.sleep(5)
             fill_form(page)
+            verification_by_phone_erip(page)
         else:
             print(tag_info)
             page.close()
