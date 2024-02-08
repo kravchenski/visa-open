4fefc04c09762e0864f646c58aa34b42a98761e3
kravchenski <max5045955@gmail.com>
2024-02-08 17:03:10 +0300

    init: create a function verification_by_phone_erip

---
+++ verification.py
@@ -0,0 +1,14 @@
+import time
+
+from DrissionPage._pages.chromium_page import ChromiumPage
+
+from config import identification_number, country_code, phone_number
+
+def verification_by_phone_erip(page):
+    page.new_tab('https://ticketing.raschet.by/vfs/web/login')
+    phone_verification_page = page.get_tab(page.latest_tab)
+    time.sleep(5)
+    phone_verification_page.ele('xpath:/html/body/div[1]/div/form/fieldset/div[1]/input').input(identification_number)
+    phone_verification_page.ele('xpath:/html/body/div[1]/div/form/fieldset/div[2]/div/input').input(country_code + phone_number)
+    phone_verification_page.ele('xpath://*[@id="buttons"]/button[1]').click()
+
