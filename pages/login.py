ef7bc90ad416a083cf2c093d7e3cb7aefd9ec268
kravchenski <max5045955@gmail.com>
2025-06-16 19:56:00 +0200

    feat: add some changes into login page

--- pages/login.py
+++ pages/login.py
@@ -1,6 +1,7 @@
 import os
 
-from utils import is_cloudflare_bypass
+from DrissionPage._functions.keys import Keys
+from utils import is_cloudflare_bypass, is_loader_hide
 
 
 def login_to_vfs(page):
@@ -14,3 +15,7 @@ def login_to_vfs(page):
     is_cloudflare_bypass(page)
     page.ele(
         'xpath:/html/body/app-root/div/main/div/app-login/section/div/div/mat-card/form/button', timeout=60).click()
+    page.ele(
+        'xpath:/html/body/app-root/div/main/div/app-dashboard/section[1]/div/div[1]/div[2]/button', timeout=60).input(
+        Keys.ENTER)
+    is_loader_hide(page)
