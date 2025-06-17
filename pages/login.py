8a06e0046244ed027a9499f81525e915518aae5f
kravchenski <max5045955@gmail.com>
2025-06-17 15:46:05 +0200

    feat: add button cookies check

--- pages/login.py
+++ pages/login.py
@@ -8,7 +8,9 @@ def login_to_vfs(page):
     email = os.environ['email_login']
     password = os.environ['password_login']
     page.get(page.base_url)
-    page.ele('xpath:/html/body/div[2]/div[2]/div/div/div[2]/div/div/button[1]', timeout=30).click()
+    ele = page.ele('xpath:/html/body/div[2]/div[2]/div/div/div[2]/div/div/button[1]', timeout=30)
+    if ele is not None:
+        ele.click()
     page.ele('xpath://*[@id="email"]', timeout=60).input(email)
     page.ele('xpath://*[@id="password"]', timeout=60).input(password)
 
@@ -16,6 +18,7 @@ def login_to_vfs(page):
     page.ele(
         'xpath:/html/body/app-root/div/main/div/app-login/section/div/div/mat-card/form/button', timeout=60).click()
     page.ele(
-        'xpath:/html/body/app-root/div/main/div/app-dashboard/section[1]/div/div[1]/div[2]/button', timeout=60).input(
+        'xpath:/html/body/app-root/div/main/div/app-dashboard/section[1]/div/div[1]/div[2]/button',
+        timeout=60).input(
         Keys.ENTER)
     is_loader_hide(page)
