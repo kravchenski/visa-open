af4f11a488255a9a9f976e7f9963692cd44a71cc
kravchenski <max5045955@gmail.com>
2025-06-17 15:47:05 +0200

    feat: run script script every 10 minutes

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import time
 
 from config.page import VisaOpenPage
-from pages.check_dates import check_dates
+from pages.check_dates_for_all_visa_types_for_one_city import check_dates_for_all_visa_types_for_one_city
+from pages.fill_form import fill_form
 from pages.login import login_to_vfs
 
 start = time.time()
 
 page = VisaOpenPage.create(mode='incognito')
-
 if __name__ == '__main__':
-    login_to_vfs(page)
-    check_dates(page)
-    print(time.time() - start)
+    while True:
+        page.clear_cache()
+
+        login_to_vfs(page)
+        check_dates_for_all_visa_types_for_one_city(page)
+        fill_form(page)
+        time.sleep(60)
